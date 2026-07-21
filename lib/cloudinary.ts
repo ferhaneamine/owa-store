c25abbbca34d54bf6aee98a463023a72d1110dc7
amine ferhane <ferhaneamine03@gmail.com>
2026-07-21 02:45:01 +0100

    Remove Cloudinary debug log

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -6,12 +6,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
-export default cloudinary;
-console.log({
-  cloud: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secretLength: process.env.CLOUDINARY_API_SECRET?.length,
-});
 export async function uploadImage(fileBase64: string, folder = "owa/products") {
   const result = await cloudinary.uploader.upload(fileBase64, {
     folder,
